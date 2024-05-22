0bc3e5a155e4de1b33d35a01f544d653cf83ad20
CHia <dozeuto@gmail.com>
2024-05-22 13:57:12 +0800

    1.7: unicafe step 2
    (cherry picked from commit 8f702d6788b08b4e100b0263c7ab9005f6c32cf9)

--- frontend/src/part1/unicafe/Unicafe.jsx
+++ frontend/src/part1/unicafe/Unicafe.jsx
@@ -5,23 +5,27 @@ export default function Unicafe() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = good + neutral + bad;
+  const average = (good * 1 + neutral * 0 + bad * -1) / all;
+  const positive = (good / all) * 100;
+
   return (
     <div>
       <h1>give feedback</h1>
       <button
-        className="mx-8 border-black border-2"
+        className="mx-1 px-1 border-black border-2 rounded"
         onClick={() => setGood(good + 1)}
       >
         good
       </button>
       <button
-        className="mx-8 border-black border-2"
+        className="mx-1 px-1  border-black border-2 rounded"
         onClick={() => setNeutral(neutral + 1)}
       >
         neutral
       </button>
       <button
-        className="mx-8 border-black border-2"
+        className="mx-1 px-1 border-black border-2 rounded"
         onClick={() => setBad(bad + 1)}
       >
         bad
@@ -30,6 +34,9 @@ export default function Unicafe() {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   );
 }
