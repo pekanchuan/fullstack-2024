75daa927e58d5065b25c0379927243e4ad08ef5b
CHia <dozeuto@gmail.com>
2024-05-22 14:27:25 +0800

    1.10: unicafe step 5
    (cherry picked from commit cf6ff574da45bd983d2950b83b74eb5fe0a3ba68)

---
+++ frontend/src/part1/unicafe/components/StatisticLine.jsx
@@ -0,0 +1,9 @@
+export default function ({ text, value }) {
+  return (
+    <>
+      <p>
+        {text} {value}
+      </p>
+    </>
+  );
+}
--- frontend/src/part1/unicafe/components/Statistics.jsx
+++ frontend/src/part1/unicafe/components/Statistics.jsx
@@ -1,3 +1,5 @@
+import StatisticLine from "./StatisticLine";
+
 export default function Statistics({ good, neutral, bad }) {
   const all = good + neutral + bad;
   const average = (good * 1 + neutral * 0 + bad * -1) / all;
@@ -15,12 +17,12 @@ export default function Statistics({ good, neutral, bad }) {
   return (
     <>
       <h1 className="m-2 text-3xl">statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={`${positive}%`} />
     </>
   );
 }
